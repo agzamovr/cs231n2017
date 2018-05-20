Fixed gradient calculation for parameters in first hidden layer

assignment2/cs231n/classifiers/fc_net.py
@@ -22,7 +22,7 @@ class TwoLayerNet(object):
     self.params that maps parameter names to numpy arrays.
     """
 
-    def __init__(self, input_dim=3*32*32, hidden_dim=100, num_classes=10,
+    def __init__(self, input_dim=3 * 32 * 32, hidden_dim=100, num_classes=10,
                  weight_scale=1e-3, reg=0.0):
         """
         Initialize a new network.
@@ -54,7 +54,6 @@ class TwoLayerNet(object):
         ############################################################################
         #                             END OF YOUR CODE                             #
         ############################################################################
-
 
     def loss(self, X, y=None):
         """
@@ -112,7 +111,7 @@ class TwoLayerNet(object):
         # backprop the ReLU non-linearity
         dhidden[relu <= 0] = 0
         # finally into W,b
-        grads['W1'] = X.T.dot(dhidden)
+        grads['W1'] = X.reshape(X.shape[0], -1).T.dot(dhidden)  # affine_forward(X.T, dhidden, 0)
         grads['b1'] = np.sum(dhidden, axis=0)
 
         # add regularization gradient contribution
@@ -141,7 +140,7 @@ class FullyConnectedNet(object):
     self.params dictionary and will be learned using the Solver class.
     """
 
-    def __init__(self, hidden_dims, input_dim=3*32*32, num_classes=10,
+    def __init__(self, hidden_dims, input_dim=3 * 32 * 32, num_classes=10,
                  dropout=0, use_batchnorm=False, reg=0.0,
                  weight_scale=1e-2, dtype=np.float32, seed=None):
         """
@@ -209,7 +208,6 @@ class FullyConnectedNet(object):
         # Cast all parameters to the correct datatype
         for k, v in self.params.items():
             self.params[k] = v.astype(dtype)
-
 
     def loss(self, X, y=None):
         """