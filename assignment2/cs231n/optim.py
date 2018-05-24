5617db4e77576f98e3764f379782548340e1cd6f
Rustam Agzamov <agzamovr@gmail.com>
2018-05-24 22:47:15 +0500

    Implemented Adam algorithm

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -105,6 +105,7 @@ def rmsprop(x, dx, config=None):
     ###########################################################################
     cache = decay_rate * cache + (1 - decay_rate) * dx ** 2
     next_x = x - learning_rate * dx / (np.sqrt(cache) + eps)
+    config['cache'] = cache
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -135,13 +136,28 @@ def adam(x, dx, config=None):
     config.setdefault('v', np.zeros_like(x))
     config.setdefault('t', 1)
 
-    next_x = None
+    learning_rate = config['learning_rate']
+    beta1 = config['beta1']
+    beta2 = config['beta2']
+    eps = config['epsilon']
+    m = config['m']
+    v = config['v']
+    t = config['t']
+
     ###########################################################################
     # TODO: Implement the Adam update formula, storing the next value of x in #
     # the next_x variable. Don't forget to update the m, v, and t variables   #
     # stored in config.                                                       #
     ###########################################################################
-    pass
+    m = beta1 * m + (1 - beta1) * dx
+    mt = m / (1 - beta1 ** t)
+    v = beta2 * v + (1 - beta2) * (dx ** 2)
+    vt = v / (1 - beta2 ** t)
+    next_x = x - learning_rate * mt / (np.sqrt(vt) + eps)
+
+    config['m'] = m
+    config['v'] = v
+    config['t'] = t
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
